161c69780ba774c7c223419b089f47c28902dad4
VC920 <3359924317@qq.com>
2026-04-12 11:19:37 +0800

    first commit

---
+++ build/CMakeFiles/renderer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/renderer.dir/link.d"
+  "CMakeFiles/renderer.dir/main.cpp.o"
+  "CMakeFiles/renderer.dir/main.cpp.o.d"
+  "CMakeFiles/renderer.dir/thirdparty/glad/src/glad.c.o"
+  "CMakeFiles/renderer.dir/thirdparty/glad/src/glad.c.o.d"
+  "renderer"
+  "renderer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/renderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
